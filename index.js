720a3f0846d4dae04f2f22a2214f9615b2506463
BytsaiKonstantin <91729673+kbytsai@users.noreply.github.com>
2024-05-21 14:05:00 +0300

    Render btn added
    Added button for render fractal and refactor render in function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("canvas");
 let currentDot;
+const renderBtn = document.querySelector(".renderbtn");
+
 const startPoint = {
   x: Math.floor(Math.random() * 1900),
   y: Math.floor(Math.random() * 1000),
@@ -36,29 +38,33 @@ function FindNextDot(apex, prevDot) {
   return { x: dotX, y: dotY };
 }
 
-// Check browser support
-if (canvas.getContext) {
-  const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#c71f1f";
+function renderFractal() {
+  // Check browser support
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#c71f1f";
 
-  //draw apexs of triangle
-  ctx.fillRect(dotA.x, dotA.y, 1, 1);
-  ctx.fillRect(dotB.x, dotB.y, 1, 1);
-  ctx.fillRect(dotC.x, dotC.y, 1, 1);
+    //draw apexs of triangle
+    ctx.fillRect(dotA.x, dotA.y, 1, 1);
+    ctx.fillRect(dotB.x, dotB.y, 1, 1);
+    ctx.fillRect(dotC.x, dotC.y, 1, 1);
 
-  //draw first dot
-  ctx.fillRect(startPoint.x, startPoint.y, 1, 1);
+    //draw first dot
+    ctx.fillRect(startPoint.x, startPoint.y, 1, 1);
 
-  for (let index = 0; index < 100000; index++) {
-    setTimeout(() => {
-      index == 0 ? (currentDot = startPoint) : null;
-      let NextApex = CheckNextApex();
-      currentDot = FindNextDot(NextApex, currentDot);
+    for (let index = 0; index < 100000; index++) {
+      setTimeout(() => {
+        index == 0 ? (currentDot = startPoint) : null;
+        let NextApex = CheckNextApex();
+        currentDot = FindNextDot(NextApex, currentDot);
 
-      ctx.fillRect(currentDot.x, currentDot.y, 1, 1);
-    }, 1);
+        ctx.fillRect(currentDot.x, currentDot.y, 1, 1);
+      }, 1);
+    }
+  } else {
+    document.querySelector(".container").textContent =
+      "Sorry your browser doesn't support my app";
   }
-} else {
-  document.querySelector(".container").textContent =
-    "Sorry your browser doesn't support my app";
 }
+
+renderBtn.addEventListener("click", renderFractal);
